74fc05e94c1f577dd2a5653f6be242cc1f339f9d
Luke Channings <luke@channings.me>
2017-02-24 00:22:54 +0000

    Update tests.

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -3,34 +3,43 @@
 const minimist = require('minimist')
 
 const {
-  getLanguage,
-  getApiChoice
+  getOptions
 } = require('../lib/cli')
 
 describe('command line interface', () => {
   describe('language arguments', () => {
-    it('-l ru', () => {
-      const m = minimist(['-l', 'ru'])
-      expect(getLanguage(m)).toBe('ru')
+    it('-l ru input', () => {
+      const m = minimist('-l ru input'.split(' '))
+      expect(getOptions(m).language).toBe('ru')
     })
-    it('--lang gb', () => {
-      const m = minimist(['--lang', 'gb'])
-      expect(getLanguage(m)).toBe('gb')
+    it('-l gb input', () => {
+      const m = minimist('-l gb input'.split(' '))
+      expect(getOptions(m).language).toBe('gb')
     })
-    it('--language ie', () => {
-      const m = minimist(['--language', 'ie'])
-      expect(getLanguage(m)).toBe('ie')
+    it('-l ie input', () => {
+      const m = minimist('-l ie input'.split(' '))
+      expect(getOptions(m).language).toBe('ie')
     })
   })
 
   describe('api arguments', () => {
+    it('defaults', () => {
+      const m = minimist('-l foo input'.split(' '))
+      const {api, apiKey} = getOptions(m)
+      expect(api).toEqual('google')
+      expect(apiKey).toEqual(undefined)
+    })
     it('--translator yandex --apiKey abc123', () => {
-      const m = minimist('--translator yandex --apiKey abc123'.split(' '))
-      expect(getApiChoice(m)).toEqual({api: 'yandex', 'apiKey': 'abc123'})
+      const m = minimist('-l foo input --translator yandex --apiKey abc123'.split(' '))
+      const {api, apiKey} = getOptions(m)
+      expect(api).toEqual('yandex')
+      expect(apiKey).toEqual('abc123')
     })
     it('--t yandex --key def456', () => {
-      const m = minimist('--t yandex --key def456'.split(' '))
-      expect(getApiChoice(m)).toEqual({api: 'yandex', 'apiKey': 'def456'})
+      const m = minimist('-l foo input --t yandex --key def456'.split(' '))
+      const {api, apiKey} = getOptions(m)
+      expect(api).toEqual('yandex')
+      expect(apiKey).toEqual('def456')
     })
   })
 })
